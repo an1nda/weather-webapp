81cc9a61572ad3695d5390faebdf1bc97e59d2fd
an1nda <aninda.saha27@gmail.com>
2023-05-25 00:19:10 -0400

    start outfit selector

--- src/App.js
+++ src/App.js
@@ -11,20 +11,29 @@ const api = {
   key: `1422ec8bf755c0a280635dbafe8e1d81`,
   base: `https://api.openweathermap.org/data/2.5/weather?`
 }
+const headwear = ['a baseball cap', 'sunglasses',
+                  'a bucket hat', 'a beanie', 'earmuffs',
+                  'a hood', 'toque']
+const outerwear = ['a vest,', 'a hoodie,', 'a windbreaker,',
+                  'a bubble jacket,', 'a winter jacket,']
+const top = ['a tank top,' ,'a t-shirt,', 'a polo shirt,',
+             'a sweater,', 'a long sleeve shirt,']
+const bot = ['shorts,', 'cargo pants,', 'sweatpants,',
+              'khakis,', 'thick sweatpants,', 'jeans,']
+const feet = ['and flip flops.', 'and sandals.', 'and runners.', 
+              'and sneakers.','and rain boots.', 'and snow boots.']
 
-const LoadingIndicator = props => {
-  const { promiseInProgress } = usePromiseTracker();
-  return (
-    promiseInProgress &&
-    <h1>async call in progress</h1>
-  )
-}
-
+const starter = ["You should wear ",
+                "A perfect outfit for today would be ",
+                "A lovely combination for today would be ",
+                "Today is the perfect day to wear ",
+                "The outfit of the day is ",
+                "Now is the time to wear ",
+                "Outfit of the day starts with "]
 
 function App() {
   
   const [weatherData, setWeatherData] = useState(null)
-  const [isLoaded, setIsLoaded] = useState(false)
 
   useEffect(() => {
     const fetchWeatherData = async () => {  
@@ -40,7 +49,6 @@ function App() {
     }
 
     fetchWeatherData();
-    setIsLoaded(true);
   }, [])
 
   const getCurrentLocation = () => {
@@ -55,12 +63,60 @@ function App() {
     console.log(data)
     return data;
   }
+
+  const getTemperatureResponse = () => {
+    if (weatherData && weatherData.main && weatherData.main.temp) {
+      const temp = Math.round(weatherData.main.temp-273.15);
+      const condition = weatherData.weather[0].id / 100;
+      const severity = 
+        weatherData.weather[0].description.includes("heavy") ||
+        weatherData.weather[0].description.includes("extreme") ||
+        weatherData.weather[0].description.includes("heavy");
+
+      if(severity){
+        return(
+          <h2>Stay inside!!!</h2>
+        )
+      } else {
+        if(condition === 3 || condition === 5){ //Rainy
+          checkTemp(temp)
+        } else if (condition === 6) { //Snowy
+          checkTemp(temp)
+        } else { // Any other condition
+          checkTemp(temp)
+        }
+      }
+    }
+
+    return '';
+  };
+
+  const checkTemp = (temp) => {
+    if(temp > 15){
+      return (
+        <span>{`${starter[Math.floor(Math.random(0, starter.length))] +
+              top[[Math.floor(Math.random(0,2))]] + 
+              bot[[Math.floor(Math.random(0,3))]] +
+              feet[[Math.floor(Math.random(0,4))]]}
+              Add in ${headwear[Math.floor(Math.random(0,3))]} 
+              and you are set!`}</span>
+      )
+    } else if(temp > 7) {
+      
+    } else if (temp > -7) {
+      
+    } else if (temp > -10){
+      
+    } else {
+      
+    }
+  }
   
   return (
     <div className="App">
       <header className="App-header">
         <div className='container'>
-          <h1>How's it looking outside?</h1>
+          <h1 className='title'>How's it looking outside?</h1>
           <div>
             {weatherData ? (
               <div>
