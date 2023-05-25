b195407ab76035d95d26785d0b5ad6a450de2ded
an1nda <aninda.saha27@gmail.com>
2023-05-24 23:09:04 -0400

    Fixed API Calls, adding animations

--- src/App.js
+++ src/App.js
@@ -1,50 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from "react";
+import { BarLoader } from 'react-spinners';
+import './index.css';
 import axios from "axios";
+import { usePromiseTracker } from "react-promise-tracker";
+import { trackPromise } from 'react-promise-tracker';
 
 const api = {
   key: `1422ec8bf755c0a280635dbafe8e1d81`,
   base: `https://api.openweathermap.org/data/2.5/weather?`
 }
 
+const LoadingIndicator = props => {
+  const { promiseInProgress } = usePromiseTracker();
+  return (
+    promiseInProgress &&
+    <h1>async call in progress</h1>
+  )
+}
+
+
 function App() {
   
-  const [lat, setLat] = useState("");
-  const [lon, setLon] = useState("");
+  const [weatherData, setWeatherData] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
 
-  const[weather, setWeather] = useState({})
+  useEffect(() => {
+    const fetchWeatherData = async () => {  
+      try {  
+        const pos = await getCurrentLocation();
 
-  React.useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position.coords)
-      setLat(position.coords.latitude);
-      setLon(position.coords.longitude);
+        const weather = await fetchWeather(pos.coords.latitude, pos.coords.longitude)
+        
+        setWeatherData(weather)
+      } catch (error) {
+        console.error("Error fetching weather data:", error)
+      }
+    }
+
+    fetchWeatherData();
+    setIsLoaded(true);
+  }, [])
+
+  const getCurrentLocation = () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject)
     })
+  }
 
-    let apiCall = `${api.base}lat=${lat}&lon=${lon}&appid=${api.key}`;
-
-    fetch(apiCall)
-    .then((response) => response.json())
-    .then((res) => {
-      console.log(res)
-      setWeather(res)
-    })
-  }, [lat, lon])
+  const fetchWeather = async (latitude, longitude) => {
+    const res = await fetch(`${api.base}lat=${latitude}&lon=${longitude}&appid=${api.key}`);
+    const data = await res.json();
+    console.log(data)
+    return data;
+  }
   
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather App</h1>
-        <div>
-          <h3>{weather.name}</h3>
+        <div className='container'>
+          <h1>How's it looking outside?</h1>
+          <div>
+            {weatherData ? (
+              <div>
+                <h5>Currently in {weatherData.name}, the temperature is</h5>
+                <h3>{Math.round(weatherData.main.temp - 273.15)} °C.</h3>
+                <h5>It's looking like {weatherData.weather[0].description}!</h5>
+
+              </div>
+            ) : (
+              <BarLoader className='loader' color="#010101" loading={true} />
+            )}
+          </div>
         </div>
-
-        <p>{weather.weather[0].main}</p>
-
-        <p>{} °C</p>
-
-        <p>Sunny</p>
       </header>
     </div>
   );
